3446866cbba089fbe62dbe04132952e6bc0aa701
Taiwola <seunolanitori@gmail.com>
2023-08-05 06:51:40 +0100

    lesson entity added

--- src/modules/lesson/entities/lesson.entity.ts
+++ src/modules/lesson/entities/lesson.entity.ts
@@ -1,10 +1,32 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Column } from 'typeorm';
 import { Tutorial } from '../../tutorial/entities/tutorial.entity';
 
 @Entity()
 export class Lesson {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({
+    nullable: true,
+    array: true,
+    type: 'simple-array',
+  })
+  images: string[] | null;
+
+  @Column({
+    nullable: true,
+    type: 'text',
+  })
+  image: string | null;
+
+  @Column({ nullable: true, type: 'varchar' })
+  video: string | null;
+
+  @Column({ nullable: true, type: 'text' })
+  text: string | null;
+
+  @Column({ nullable: true, type: 'text' })
+  document: string | null;
 
   @ManyToOne(() => Tutorial, (tutorial) => tutorial.lessons)
   tutorial: Tutorial;
